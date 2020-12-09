7d9d1adac5e5e605826ff62063bd0d547d4b542a
jameson1047 <james.cummings47@gmail.com>
2020-12-09 15:10:29 -0500

    Deleted main.storyboard

--- nightowls/PartyTableViewCell.swift
+++ nightowls/PartyTableViewCell.swift
@@ -9,15 +9,32 @@ import UIKit
 
 class PartyTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private let containerView = UIView()
+    private let partyImageView = UIImageView()
+    private let partyNameLabel = UILabel()
+    private let startTimeLabel = UILabel()
+    private let ratingLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        backgroundColor = UIColor(red: 239/255, green: 239/255, blue: 239/255, alpha: 1)
+        
+        setupViews()
+        setupConstraints()
+        
+}
+    
+    func setupViews() {
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func setupConstraints() {
+        
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
