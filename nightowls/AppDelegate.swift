c4e668005aecb81250b33b6fb5febb1083da1d2c
jameson1047 <james.cummings47@gmail.com>
2020-12-15 19:08:24 -0500

    Last minute updates and formatting changes

--- nightowls/AppDelegate.swift
+++ nightowls/AppDelegate.swift
@@ -31,6 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    //credit to cornell app dev's open source code that was used throughout this course as a reference while building this app.
 
 }
 
